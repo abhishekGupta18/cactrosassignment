1ea52e375fe73154e4f0d055be102b246b789122
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2025-04-09 01:44:27 +0530

    created auth.js middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const Client = require("../models/client");
+
+const clientAuth = async (req, res, next) => {
+  try {
+    const { token } = reqcookies;
+    if (!token) {
+      return res.status(401).send("please login");
+    }
+
+    const decodedTokenData = await jwt.verify(
+      token,
+      process.env.jwt_token_secret
+    );
+    const { _id } = decodedTokenData;
+
+    const client = await Client.findById(_id);
+    if (!client) {
+      throw new Error("user not found !!");
+    } else {
+      req.client = client;
+      next();
+    }
+  } catch (e) {
+    res.status(400).send("Error: " + e.message);
+  }
+};
+
+module.exports = clientAuth;
