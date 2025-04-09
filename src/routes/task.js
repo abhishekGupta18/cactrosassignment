c7dc4321f3c28686f5c408b6eff4f52fe8a5561b
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2025-04-09 07:19:58 +0530

    completed add taskRouter api

--- src/routes/task.js
+++ src/routes/task.js
@@ -27,4 +27,64 @@ taskRouter.post("/task/create", clientAuth, async (req, res) => {
   }
 });
 
+// getting all the task of loggedIn cient/user
+
+taskRouter.get("/tasks", clientAuth, async (req, res) => {
+  try {
+    const loggedInClient = req.client;
+
+    const allTasks = await Task.find({ clientId: loggedInClient._id }).sort({
+      createdAt: -1,
+    });
+    res.status(200).json({
+      msg: "all tasks of loggedIn client",
+      data: allTasks,
+    });
+  } catch (e) {
+    return res.status(500).json({ msg: e.message });
+  }
+});
+
+// update the title, status or description
+taskRouter.patch("/task/update/:id", clientAuth, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updates = req.body;
+
+    const task = await Task.findById(id);
+    if (!task) {
+      return res.status(404).json({ msg: "Task not found" });
+    }
+
+    const updatedTask = await Task.findByIdAndUpdate(id, updates, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      msg: "Task updated successfully",
+      data: updatedTask,
+    });
+  } catch (e) {
+    return res.status(500).json({ msg: e.message });
+  }
+});
+
+// deleting a task
+
+taskRouter.delete("/delete/task/:id", clientAuth, async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deletedTask = await Task.findByIdAndDelete(id);
+
+    res.status(200).json({
+      msg: "Task deleted successfully",
+      data: deletedTask,
+    });
+  } catch (e) {
+    return res.status(500).json({ msg: e.message });
+  }
+});
+
 module.exports = taskRouter;
